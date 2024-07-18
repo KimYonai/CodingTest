91994688175eee0b0231a541ed42a86cdf76ad92
KimYonai <msb0714@gmail.com>
2024-07-18 17:45:38 +0900

    Test_02
    테스트2. 두 수의 곱

--- Program.cs
+++ Program.cs
@@ -4,13 +4,25 @@
     {
         #region 문제1. 두 수의 차
         //문제: 정수 num1과 num2가 주어질 때, num1에서 num2를 뺀 값을 return하도록 solution 함수를 완성해주세요.
-        public int solution(int num1, int num2)                 //1. solution 함수에서 정수 num1과 num2 선언
-        {
-            int answer = num1 - num2;                           //2. num1과 num2를 마이너스 연산 실행
-            return answer;                                      //3. num1과 num2의 마이너스 연산값 도출
+        //public int solution(int num1, int num2)                 //1. solution 함수에서 정수 num1과 num2 선언
+        //{
+        //    int answer = num1 - num2;                           //2. num1과 num2를 마이너스 연산 실행
+        //    return answer;                                      //3. num1과 num2의 마이너스 연산값 도출
+        //
+        //    Console.WriteLine($"Answer: {num1 - num2}");        //4. num1과 num2의 마이너스 연산값을 출력
+        //}
+        #endregion
 
-            Console.WriteLine($"Answer: {num1 - num2}");        //4. num1과 num2의 마이너스 연산값을 출력
-        }
+        #region 문제2. 두 수의 곱
+        //문제: 정수 num1, num2가 매개변수 주어집니다.num1과 num2를 곱한 값을 return 하도록 solution 함수를 완성해주세요.
+
+        //public int solution(int num1, int num2)                //1. solution 함수에서 정수 num1과 num2 선언    
+        //{
+        //    int answer = num1 * num2;                          //2. num1과 num2를 곱연산 실행
+        //    return answer;                                     //3. num1과 num2의 곱 연산값 도출
+        //
+        //    Console.WriteLine($"Answer: {num1 * num2}");       //4. num1과 num2의 곱 연산값을 출력
+        //}
         #endregion
         static void Main(string[] args)
         {
