64219605e4b5b620a26083835db2548949913cd3
KimYonai <msb0714@gmail.com>
2024-07-19 15:45:10 +0900

    Day_01
    프로그래머스 코딩테스트 1일차
    -현재 난이도: 입문

--- Program.cs
+++ Program.cs
@@ -2,7 +2,18 @@
 {
     internal class Program
     {
-        #region 문제1. 두 수의 차
+        #region 문제1. 두 수의 합
+        //문제: 정수 num1과 num2가 주어질 때, num1과 num2의 합을 return하도록 solution 함수를 완성해주세요.
+        //public int solution(int num1, int num2)                   //1. solution 함수에서 정수 num1과 num2 선언
+        //{
+        //    int answer = num1 + num2;                             //2. num1과 num2를 더하기 연산 실행
+        //    return answer;                                        //3. num1과 num2의 더하기 연산값 도출
+        //
+        //    Console.WriteLine($"Answer: {num1 + num2}");          //4. num1과 num2의 더하기 연산값을 출력
+        //}
+        #endregion
+
+        #region 문제2. 두 수의 차
         //문제: 정수 num1과 num2가 주어질 때, num1에서 num2를 뺀 값을 return하도록 solution 함수를 완성해주세요.
         //public int solution(int num1, int num2)                 //1. solution 함수에서 정수 num1과 num2 선언
         //{
@@ -13,7 +24,7 @@
         //}
         #endregion
 
-        #region 문제2. 두 수의 곱
+        #region 문제3. 두 수의 곱
         //문제: 정수 num1, num2가 매개변수 주어집니다.num1과 num2를 곱한 값을 return 하도록 solution 함수를 완성해주세요.
 
         //public int solution(int num1, int num2)                //1. solution 함수에서 정수 num1과 num2 선언    
@@ -25,7 +36,21 @@
         //}
         #endregion
 
-        #region 문제3. 숫자 비교하기
+        #region 문제4. 몫 구하기
+        //문제: 정수 num1, num2가 매개변수 주어질 때, num1을 num2로 나눈 몫을 return 하도록 solution 함수를 완성해주세요.
+        //public int solution(int num1, int num2)                  //1. solution 함수에서 정수 num1과 num2 선언
+        //{
+        //    int answer = num1 / num2;                            //2. num1과 num2를 나누기 연산 실행
+        //    return answer;                                       //3. num1과 num2의 나누기 연산으로 인한 몫만 연산값으로 출력
+        //}
+        #endregion
+
+        #region 문제5. 두 수의 나눗셈
+        //
+
+        #endregion
+
+        #region 문제6. 숫자 비교하기
         //문제: 정수 num1과 num2가 매개변수로 주어집니다. 두 수가 같으면 1, 다르면 -1을 return하도록 solution 함수를 완성해주세요.
 
         //public int solution(int num1, int num2)       //1. solution 함수에서 정수 num1과 num2 선언
@@ -42,6 +67,8 @@
         //    return answer;                            //5. 조건에 따른 결과값 도출
         //}
         #endregion
+
+
         static void Main(string[] args)
         {
  
